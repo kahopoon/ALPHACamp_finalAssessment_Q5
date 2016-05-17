ad0fab9559e1ebe3b1db7fbb00abe5ce6e625fae
Johnson Kaho Poon <kahopoon@gmail.com>
2016-05-17 16:27:07 +0800

    pod installed

---
+++ Pods/Target Support Files/Pods-ALPHACamp_finalAssessment_Q5/Pods-ALPHACamp_finalAssessment_Q5-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALPHACamp_finalAssessment_Q5 : NSObject
+@end
+@implementation PodsDummy_Pods_ALPHACamp_finalAssessment_Q5
+@end
---
+++ Pods/Target Support Files/Pods-ALPHACamp_finalAssessment_Q5/Pods-ALPHACamp_finalAssessment_Q5-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ALPHACamp_finalAssessment_Q5VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ALPHACamp_finalAssessment_Q5VersionString[];
+
